686c774440677d2827056dbe7e8379e401b6daba
dhanesh9 <56043013+dhanesh9@users.noreply.github.com>
2023-04-03 08:46:27 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,45 @@
+Name - Dhanesh Akolu
+
+login - dakolu@stevens.edu
+
+URL: https://github.com/dhanesh9/CS515
+
+
+Description of the game-
+
+Welcome to a thrilling adventure game! In this game, your mission is to find the hidden treasure in a room. However, there's a twist: you need to have a gun to take down the guard who's protecting the treasure, and a key to unlock the treasure chest. Don't forget to enter "get treasure" to add both the key and treasure to your inventory and win the game.
+
+But be careful, there's a deadly shaft in the map. If you fall into it without a rope, you lose the game. So, be sure to stay alert and make wise choices. Are you ready for the challenge? Let's begin the game and see if you can succeed in this exciting quest!
+
+
+
+1)an estimate of how many hours you spent on the project- 
+- 8-10 hours
+
+2)a description of how you tested your code
+
+-I created different maps and kept testing all the maps including the one on Canvas. Finally, when I thought about my final map I implemented the win/lose conditions for the map
+and checked all the edge cases. I executed my code using both command prompt and visual studio. I made sure everything works perfectly with the items, inventory and current room.
+
+3)any bugs or issues you could not resolve
+
+- I wasn't able to handle the EOFError with ^D but when I read about it online it said that sometimes on windows ^D cannot trigger EOFError but ^Z does. SO when I check with 
+  ^Z it worked well.
+
+4)an example of a difficult issue or bug and how you resolved
+
+- I wasn't able to run the code without passing the json file path but I had to read a lot to find out about 'argparse'. I fixed it using argparse
+
+5)a list of the three extensions you’ve chosen to implement, with appropriate detail on them for the CAs to evaluate them (i.e., what are the new verbs/features, how do you exercise them, where are they in the map)
+
+- I implemented drop(), help() and winning losing condition.
+  i)Drop()- with the drop function we can drop the items from the inventory one by one. Once you drop the item in a specific room, that particular item gets appended in the current room's
+          item list.
+  ii)help() - this function shows all the commands that you can perform while playing the game.
+  iii)Winning losing condition- Basically, when you reach the treasure room you need to have the key in your inventory to open the treasure chest and a gun to kill the guard who is 
+	protecting the treasure. You can even kill the guard first and then go get the key from storage room, then traverse back to the treasure room and win the game. Remember that you need
+	to use say 'get treasure' so that treasure and key are both in your inventory to win the game.
+	For losing condition, I have implemented a shaft room, if you fall in the shaft room without picking up the rope which is in the armory room, you will lose. But if you have the rope 
+	and you fall in the shaft you can survive and then use the exits in the shaft to traverse.
+
+
---
+++ adventure.py
@@ -0,0 +1,145 @@
+import json
+import inspect
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("map_file", help="Path to the map file")
+args = parser.parse_args()
+
+class Game:
+    def __init__(self, args):
+        with open(args.map_file, 'r') as f:
+            self.game_map = json.load(f)
+        self.current_room = self.game_map[0]
+        self.inventory = []
+        self.game_won = False
+        self.game_lost = False
+
+    def look(self):
+        if self.game_won:
+            print("You have already won the game!")
+            return
+        elif self.game_lost:
+            print("You have already lost the game!")
+            return
+        print(self.current_room["name"] + "\n")
+        print(self.current_room["desc"] + "\n")
+        if "items" in self.current_room:
+            print("Items:", ', '.join(self.current_room["items"]) + "\n")
+        print("Exits:", ', '.join(self.current_room["exits"].keys()) + "\n")
+
+    def go(self, direction):
+        if direction not in self.current_room["exits"]:
+            print("There's no way to go", direction)
+        else:
+            print("You go ",direction)
+            room_index = self.current_room["exits"][direction]
+            self.current_room = self.game_map[room_index]
+            self.look()
+        if self.current_room["name"] == "Shaft" and "rope" not in self.inventory:
+            print("You fell in the shaft and you can't get out of it so you lose")
+            self.game_lost = True
+        elif self.current_room["name"] == "Treasure Room" and "gun" not in self.inventory:
+            print("You were killed by the guard, you lose.")
+            self.game_lost = True
+
+    def get(self, item):
+        if "items" in self.current_room and item in self.current_room["items"]:
+            self.inventory.append(item)
+            self.current_room["items"].remove(item)
+            print("You pick up the", item)
+        else:
+            print("There's no", item, "here.")
+        if "keys" and "treasure" in self.inventory:
+            print("You can unlock the treasure with your key. You win!")
+            self.game_won = True
+
+    def drop(self, item):
+        if item in self.inventory:
+            self.current_room["items"].append(item)
+            self.inventory.remove(item)
+            print("You drop the", item)
+        else:
+            print("You're not carrying a", item)
+        # if item == "magic wand" and self.current_room["name"] == "Boss Room" and "sword" in self.inventory:
+        #     print("You defeat the boss with the magic wand and sword! You win!")
+        #     self.game_won = True
+
+
+    def display_inventory(self):
+        if not self.inventory:
+            print("You're not carrying anything.")
+        else:
+            print("Inventory:")
+            for item in self.inventory:
+                print("  ", item)
+
+    def help(self):
+        available_commands = [command for command in dir(Game) if not command.startswith('__') and not command == 'run']
+        print("You can run the following commands:")
+        for command in available_commands:
+            if command == 'display_inventory':
+                print("  inventory")
+            else:
+                function = getattr(self, command)
+                args = function.__code__.co_varnames[:function.__code__.co_argcount]
+                if len(args) > 1:
+                    print(f"  {command} ...")
+                else:
+                    print(f"  {command}")
+
+   
+
+    def run(self):
+        self.look()
+        while True:
+        
+            if self.game_won:
+                print("Congratulations! You won the game!")
+                break
+            elif self.game_lost:
+                print("Game over! You lost!")
+                break      
+            try:
+                command = input("What would you like to do? ").lower().split()
+            except EOFError:
+                print("Use 'quit' to exit")
+                continue
+            if not command:
+                continue
+            verb = command[0]
+            if verb == "go":
+                if len(command) < 2:
+                    print("Sorry, you need to 'go' somewhere")
+                    ans = input()
+                    if 'east' in ans:
+                        self.go('east')
+                else:
+                    self.go(command[1])
+
+            elif verb == "get":
+                if len(command) < 2:
+                    print("Sorry, you need to 'get' something.")
+                else:
+                    self.get(command[1])
+
+            elif verb == "drop":
+                if len(command) < 2:
+                    print("What do you want to drop?")
+                else:
+                    self.drop(command[1])
+
+            elif verb == "inventory":
+                self.display_inventory()
+            elif verb == "help":
+                self.help()
+            elif verb == "look":
+                self.look() 
+            elif verb == "quit":
+                print("Goodbye!")
+                break
+            else:
+                print("Sorry, I don't understand that command.")
+
+game = Game(args)
+game.run()
